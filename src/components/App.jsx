e10cb19e44df18b556f716a52b403295de8611e2
Dani Marcé Garcia <daniel.marce@mataro.epiaedu.cat>
2025-05-09 11:58:38 +0200

    feat: fetch books from backend and display BooksContainer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,28 @@
-import { HeadingTitle } from "./HeadingTitle"
+import { useEffect, useState } from "react";
+import { BooksContainer } from "./BooksContainer";
+import { HeadingTitle } from "./HeadingTitle";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const App = () => {
+  const [books, setBooks] = useState([]);
 
+  useEffect(() => {
+    const endpointUrl = `${BASE_URL}/books`;
+
+    globalThis
+      .fetch(endpointUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setBooks(data);
+      });
+  }, []);
   return (
     <>
       <HeadingTitle title="My Book Collection" />
+      <BooksContainer books={books} />
     </>
-  )
-}
+  );
+};
