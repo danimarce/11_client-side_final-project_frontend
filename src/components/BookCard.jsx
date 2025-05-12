cb94a8d4121f940bfbe87247977f41fc9fd23567
DMG <daniel.marce@mataro.epiaedu.cat>
2025-05-12 18:14:29 +0200

    feat: implement delete button

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,24 +1,45 @@
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 export const BookCard = ({ book, setBookId, handleEditButton }) => {
-  const handleBookEdit = () => {
+  const { id, title, author, year, status } = book;
+
+  const handleBookEdit = (id) => {
     handleEditButton();
-    setBookId(book.id);
+    setBookId(id);
+  };
+
+  const handleDeleteButton = (id) => {
+    const deleteBook = confirm("Are you sure you want to delete this book?");
+
+    if (!deleteBook) return;
+
+    globalThis
+      .fetch(`${BASE_URL}/books/${id}`, {
+        method: "DELETE",
+      })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
   };
 
   return (
     <li>
       <section>
         <header>
-          <h3>{book.title}</h3>
-          <span>{book.year}</span>
+          <h3>{title}</h3>
+          <span>{year}</span>
         </header>
-        <p>{book.author}</p>
+        <p>{author}</p>
       </section>
       <section>
-        <span>{book.status}</span>
+        <span>{status}</span>
       </section>
       <section>
-        <button onClick={handleBookEdit}>Edit</button>
-        <button>Delete</button>
+        <button onClick={() => handleBookEdit(id)}>Edit</button>
+        <button onClick={() => handleDeleteButton(id)}>Delete</button>
       </section>
     </li>
   );
